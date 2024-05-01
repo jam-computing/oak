d2f2524a86bba6c3504e8dd278b30562885778e2
jules white <juleswhi@gmail.com>
2024-05-01 11:41:54 +0100

    Add initial list

---
+++ cmd/oak/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/jam-computing/oak/pkg/components"
+)
+
+func main() {
+    m := components.GetModel()
+
+    p := tea.NewProgram(m, tea.WithAltScreen())
+    if _, err := p.Run(); err != nil {
+        fmt.Println("Error running program", err)
+        os.Exit(1)
+    }
+}
